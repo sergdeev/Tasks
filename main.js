f51e663c87c163ea458c57a94268550311f28019
Sergey <serg1deev@gmail.com>
2018-03-19 00:37:41 +0200

    added tasks 8-11

--- main.js
+++ main.js
@@ -7,10 +7,18 @@ reverseString("hello");
 
 //Or
 function reverseString(str) {
+   var o, i, len = str.length - 1;
+   for (var i = len, o =''; i >= 0; o+=[i--]) {}
+    return o;
+}
+reverseString("hello");
+
+//Or
+function reverseString(str) {
    var newStr = "", i;
    for (var i = str.length - 1; i >= 0; i-=1) {
-        newStr += str[i];
-    }
+     newStr += str[i];//конкатенация!
+   }
     return newStr;
 }
 reverseString("hello");
@@ -72,7 +80,52 @@ largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 85
 
 function confirmEnding(str, target) {
   var quan = target.length;
-  return (str.slice(-quan)===target)? true : false;
+  return str.slice(-quan)===target;
 }
 
 confirmEnding("Bastian", "n");
+
+
+//8. Repeat a string repeat a string
+function repeatStringNumTimes(str, num) {
+  var str2 = "", i;
+  for(i=0;i<num;i+=1){
+    str2 += str;
+  }
+  return str2;
+}
+
+repeatStringNumTimes("abc", 3);
+
+
+//9. Truncate a string
+function truncateString(str, num) {
+  if (num<=3){
+    return str.slice(0, num) + '...';
+  }
+  else if(str.length > num) {
+    return str.slice(0, num - 3) + '...';
+  }
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+//10. Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+  var newArr = [], i, n=0;
+  for(i = 0; i<Math.ceil(arr.length/size); i+=1){
+    newArr.push(arr.slice(n,n+size));
+    n+=size;
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 3);
+
+//11. Slasher Flick
+function slasher(arr, howMany) {
+  return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);
